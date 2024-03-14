9172458ee68271679bef75c27c4800f097e734db
Re-Santos <renata.prog@gmail.com>
2024-03-13 21:49:01 -0300

    Corrigi erros

--- tests/mocks/TokenManagerMock.ts
+++ tests/mocks/TokenManagerMock.ts
@@ -1,15 +1,12 @@
-
 import { HashManager } from "../../src/services/HashManager";
 import { TokenPayload, USER_ROLES } from "../../src/services/TokenManager";
 
 export class TokenManagerMock implements HashManager {
   public hash = async (plainText: string): Promise<string> => {
-
     return `hashed-${plainText}`;
   }
 
   public compare = async (plainText: string, hash: string): Promise<boolean> => {
-
     const hashedPlainText = await this.hash(plainText);
     return hashedPlainText === hash;
   }
@@ -18,23 +15,14 @@ export class TokenManagerMock implements HashManager {
     if (payload.id === "id-mock") {
       // Signup de nova conta
       return "token-mock";
-
     } else if (payload.id === "id-mock-usernormal") {
-
       // Mock para usuário normal
       return "token-mock-usernormal";
-
     } else if (payload.id === "id-mock-useradmin") {
       // Mock para usuário admin
       return "token-mock-useradmin";
     } else {
       return "";
-
-      return "token-mock-usernormal";
-
-    } else {
-
-      return "token-mock-useradmin";
     }
   }
 
--- tests/mocks/UserDataBaseMock.ts
+++ tests/mocks/UserDataBaseMock.ts
@@ -47,3 +47,4 @@ export class UserDataBaseMock extends BaseDatabase {
     return undefined;
   }
 
+}
